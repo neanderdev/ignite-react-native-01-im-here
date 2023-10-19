e142ef58eafc68de47e6ca59f60dee8217cdef02
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 10:12:58 -0300

    Fundamentos - Botão de Adicionar

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -24,4 +24,16 @@ export const styles = StyleSheet.create({
     padding: 16,
     fontSize: 16,
   },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
